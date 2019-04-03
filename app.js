81d64b854f8dcefbca07533761d118f1e253c0c1
omartaveras <omar.taverasdiaz@studenti.unicam.it>
2019-04-03 11:55:48 +0100

    -A

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 
 
+const app = express();
+
+
 //Listen the server for running in Cloud(C9, DOcean)
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server Started...  .  .  .    .");
