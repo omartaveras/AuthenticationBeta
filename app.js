f9faa159c136f20b08d07322ca9e3acbdcb52ea3
Omar Taveras <omar.t@kombinations.org>
2019-04-04 09:41:11 +0000

    updated4

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(require("express-session")({
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -57,6 +58,20 @@ app.post("/register", function(req, res){
         });
     });
 });
+
+//LOGIN Routes
+//render login form
+app.get("/login", function(req, res){
+    res.render("login");
+});
+//login logic
+app.post("/login", passport.authenticate("local",{
+        successRedirect: "/secret",
+        failureRedirect: "/login"
+    }), function(req, res){    
+});
+
+
 
 
 //Listen the server
