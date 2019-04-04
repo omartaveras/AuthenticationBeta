5f59ff6ef2cc572d79d7c3395eea2190f0d621ff
Omar Taveras <omar.t@kombinations.org>
2019-04-04 09:03:54 +0000

    update

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ mongoose.connect('mongodb://localhost:27017/auth_demo_app', { useNewUrlParser: t
 
 const app = express();
 app.set("view engine", "ejs");
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(require("express-session")({
     secret: "call me about your name",
     resave: false,
@@ -23,6 +23,10 @@ app.use(passport.session());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+//==========
+//  ROUTES
+//==========
+
 //home page
 app.get("/", function(req, res){
     res.render("home");
@@ -33,8 +37,29 @@ app.get("/secret", function(req, res){
     res.render("secret");
 });
 
+//Authe Routes
 
-//Listen the server(for LocalHost =>)
+//show sign up form
+app.get("/register", function(req, res){
+    res.render("register");
+});
+//handing sign up form
+app.post("/register", function(req, res){
+    req.body.username
+    req.body.password
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            console.res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/secret");
+        });
+    });
+});
+
+
+//Listen the server
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server Started...  .  .  .    .");
 });
